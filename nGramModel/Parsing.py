b77e2f5ea11e097e1b9379b94f361c1691f3e68f
SURESH BYSANI VENKATA NAGA <mailsureshbvn@gmail.com>
2016-04-14 20:27:09 -0700

    updated parsing

--- nGramModel/Parsing.py
+++ nGramModel/Parsing.py
@@ -10,6 +10,7 @@ class Parsing:
     def __init__(self, fileName, nValue):
         self.file_to_be_parsed = fileName
         self.outpufile = "output.txt"
+        self.outpufile_modified="ouput_modified.txt"
         self.nValue = nValue
         self.list_of_strings = []
 
@@ -40,7 +41,7 @@ class Parsing:
 
     def pre_processing(self, inputString):
 
-        punctuations = {',': ' ,COMMA', ':': ' .PEROID', '.': ' .PEROID', '!': ' .PEROID', ';': ' .PERIOD', '?': ' ?Q-MARK'}
+        punctuations = {',': ' ,COMMA ', ':': ' .PEROID ', '.': ' .PEROID ', '!': ' .PEROID ', ';': ' .PERIOD ', '?': ' ?Q-MARK ',' ':' epsilon '}
 
         changed_string = ""
         for char in inputString:
@@ -50,8 +51,40 @@ class Parsing:
                 changed_string = changed_string + punctuations[char]
         return changed_string
 
+    def modified_pre_processing(self, inputString):
+        punctuations = {',COMMA  epsilon':',COMMA','.PERIOD epsilon':'.PERIOD'}
+        print "inside"
+
+        changed_string = ""
+        for char in inputString:
+            if char not in punctuations:
+                changed_string = changed_string + char
+            else:
+                changed_string = changed_string + punctuations[char]
+                print "yes"
+        return changed_string
+    def parse_modified(self):
+        print self.file_to_be_parsed
+        log = open(self.outpufile_modified, "w")
+
+
+
+
+
+
+        with open(self.outpufile) as f:
+            for line in f:
+
+                line = line.replace(',COMMA  epsilon', ',COMMA')
+                line = line.replace('.PERIOD epsilon','.PERIOD')
+
+                log.write(line)
+
+            f.close()
+
 
 if __name__ == "__main__":
     parsing = Parsing("input.txt", 3)
     parsing.parse()
     parsing.calculate_probablities()
+    parsing.parse_modified()
